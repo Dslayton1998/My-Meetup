d282b7cc8371672dfd49de48d787702b3353818e
Dslayton1998 <dslayton1998@gmail.com>
2023-10-07 12:14:03 -0500

    Completed Edit-a-Event

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,14 +1,57 @@
 const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-const { Event, Group, Venue, EventImage  } = require('../../db/models');
+const { Event, Group, Venue, EventImage, Membership  } = require('../../db/models');
 
 //? Validating Group Request Body (& middleware)
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth.js');
 const event = require('../../db/models/event');
-const validateEvents = [];
+const validateEvents = [
+    check('venueId')
+        .custom(async val => {
+            if(val === null) return true
+
+            const venue = await Venue.findByPk(val)
+            if(!venue) {
+                throw new Error("Venue does not exist")
+            } else return true
+        }),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a name")
+        .isLength({ min: 5})
+        .withMessage("Name must be at least 5 characters"),
+    check('type')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a type.')
+        .isIn(['Online','In person'])
+        .withMessage("Type must be 'Online' or 'In person'"),
+    check('capacity')
+        .exists({ checkFalsy: true })
+        .withMessage('Please specify a capacity')
+        .isInt()
+        .withMessage("Capacity must be an integer"),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage('Please include a price')
+        .isDecimal()
+        .withMessage("Price is invalid"),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('startDate')
+        .exists({ checkFalsy: true })
+        .withMessage("starting date is required")
+        .isAfter(new Date().toLocaleDateString())
+        .withMessage("startDate must be in the future"),
+    check('endDate')
+        .exists({ checkFalsy: true })
+        .isAfter(this.startDate)
+        .withMessage("End date is less than start date"),
+    handleValidationErrors
+]
 
 const router = express.Router();
 
@@ -110,41 +153,75 @@ router.get('/:eventId', async (req, res, next) => {
     res.json(event[0])
 });
 
-
-router.put('/events/:eventId', requireAuth, async (req, res, next) => {
-    const user = req.user;
+//* EDIT AN EVENT
+router.put('/:eventId', requireAuth, validateEvents, async (req, res, next) => {
+    const user = req.user
     const { eventId } = req.params;
+    const { venueId, name, type, capacity, price, description, startDate, endDate } = req.body;
+    const venue = await Venue.findByPk(venueId);
     const events = await Event.findByPk(eventId);
-    const eventsArr = events.toJSON();
-    console.log(eventsArr)
-    //? Confirm the requested Group exists
-    if(!group) {
+
+    if(!venue) {
         return res.status(404).json({
-            "message": "Group couldn't be found"
+            message: "Venue couldn't be found"
         })
     }
 
-//? Check which members of the group have permission
-    const validUser = [];
+    if(!events) {
+          return res.status(404).json({
+            message: "Event couldn't be found"
+        })
+    }
+    const event = events.toJSON();
+
+    const group = await Group.findAll({
+        where: {
+            id: event.groupId
+        }
+    });
+
+    const organizer = group.map(ele => {
+        const arr = ele.toJSON();
+        return arr
+    });
+    // console.log("here", organizer)
+
     const getMembers = await Membership.findAll({
         where: {
-            groupId: groupId
-        }
-    });
-    const members = getMembers.map((member) => {
-        const arr = member.toJSON();
-        return arr
-        //* Array of Memberships objects
-    });
-    members.forEach(member => {
-        if(member.status === 'co-host' && user.id === member.userId) {
-            validUser.push(member)
+            status: "co-host"
         }
     })
+    // console.log(member)
+    const members = getMembers.map(ele => {
+        const arr = ele.toJSON();
+        return arr
+    });
+    
+    for(let i = 0; i < members.length; i++) {
+        const member = members[i]
+        if(user.id === organizer[0].organizerId || user.id === member.userId && organizer[0].id === member.groupId) {
+            const update = await events.update({ venueId, name, type, capacity, price, description, startDate, endDate })
+            const resObj = {
+                id: update.id,
+                groupId: update.groupId,
+                venueId: update.venueId,
+                type: update.type,
+                capacity: update.capacity,
+                price: update.price,
+                description: update.description,
+                startDate: update.startDate,
+                endDate: update.endDate
+            };
 
+            res.json(resObj);
 
-//? Check if user has Authorization('co-host', 'organizer'):
-    if(user.id === group.organizerId || validUser.length) {}
+        } else {
+            return res.status(403).json({
+                "error": "Authorization required",
+                "message": "Only group organizer, or co-host, is authorized to do that"
+            })
+        }
+    } 
 })
 
 
