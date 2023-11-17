9a9ecb38a58bbbcbaaf65630da4d1e4cbd34a5d8
Dslayton1998 <dslayton1998@gmail.com>
2023-11-17 11:19:20 -0600

    Finshed phase-2 of Auth-me

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage.jsx';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage.jsx';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginFormPage />
+      },
+      {
+        path: "/signup",
+        element: <SignupFormPage />
       }
     ]
   }
